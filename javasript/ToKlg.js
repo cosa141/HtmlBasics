c7fe8d1a1543fb8145844a837b4923de8d469c7a
cosa141 <32582921+cosa141@users.noreply.github.com>
2019-03-16 13:21:11 -0500

    getting to know javascript

---
+++ javasript/ToKlg.js
@@ -0,0 +1,4 @@
+var lbs = prompt('Enter the weight in Lbs you want to convert into Klg')
+var klg = lbs * 0.454
+alert('That is: '+ klg +' kilograms') //forgot to use + signs
+console.log('Conversion Completed')
