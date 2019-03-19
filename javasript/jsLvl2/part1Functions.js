996d0e960009c5308c74a9f5e105c4656f882f32
cosa141 <32582921+cosa141@users.noreply.github.com>
2019-03-18 20:32:32 -0500

    starting js lvl 2

---
+++ javasript/jsLvl2/part1Functions.js
@@ -0,0 +1,19 @@
+function hello() {
+  console.log('hello world');
+}
+function helloName(name) {
+  console.log('Hello '+name);
+}
+function addNum(num1,num2) {
+  console.log(num1+num2);
+}
+function helloSomeone(name='you') {
+  console.log('hello '+name );
+}
+function formal(name='Sam', title='Sir') {
+  return title+ ' '+ name
+}
+function timesFive(numInput) {
+  var result = numInput * 5
+  return result
+}
