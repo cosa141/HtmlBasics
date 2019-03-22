8037c25de2d677a529ed743e4b7543eb01ac4923
Yang M Rosado <32582921+cosa141@users.noreply.github.com>
2019-03-21 19:17:25 -0500

    finished array exercises

--- javasript/jsLvl2/Part4_Array_Exercise.js
+++ javasript/jsLvl2/Part4_Array_Exercise.js
@@ -11,22 +11,45 @@ var roster = []
 
 // Create a function called addNew that takes in a name
 // and uses .push to add a new student to the array
-
+function addNew(name) {
+  roster.push(name)
+}
 
 // REMOVE STUDENT
 
 // Create a function called remove that takes in a name
 // Finds its index location, then removes that name from the roster
-
+function remove(name) {
+  roster.pop(roster[name])
+}
 // HINT: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
 //
 
 // DISPLAY ROSTER
 
 // Create a function called display that prints out the orster to the console.
-
+function display() {
+  console.log(roster);
+}
 
 // Start by asking if they want to use the web app
-
+var play = prompt('Would you like to play? y/n')
 // Now create a while loop that keeps asking for an action (add,remove, display or quit)
 // Use if and else if statements to execute the correct function for each command.
+
+while (play=='y') {
+  var response=prompt('Would you like to add, remove, quit or view your list?')
+  if (response=='add') {
+    var adding=prompt('Enter the name you would like to add.')
+    addNew(adding)
+  }else if (response=='remove') {
+    var removing=prompt('Who would you like to remove?')
+    remove(removing)
+  }else if (response=='quit') {
+    play='heck'
+  }else if (response=='view') {
+    display()
+  }else {
+    continue
+  }
+}
