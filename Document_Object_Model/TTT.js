ac1a352472560e41f707464eddf55ab394b23042
cosa141 <32582921+cosa141@users.noreply.github.com>
2019-03-26 21:52:48 -0500

    working on tic tac toe

---
+++ Document_Object_Model/TTT.js
@@ -0,0 +1,46 @@
+var topLeft= document.querySelector('#topLeft')
+
+topLeft.addEventListener('click', function() {
+  topLeft.textContent= 'X';
+})
+
+var middleLeft= document.querySelector('#middleLeft')
+
+middleLeft.addEventListener('click', function() {
+  middleLeft.textContent= 'X';
+})
+var bottomLeft= document.querySelector('#bottomLeft')
+
+bottomLeft.addEventListener('click', function() {
+  bottomLeft.textContent= 'X';
+})
+var middle= document.querySelector('#middle')
+
+middle.addEventListener('click', function() {
+  middle.textContent= 'X';
+})
+var topMiddle= document.querySelector('#topMiddle')
+
+topMiddle.addEventListener('click', function() {
+  topMiddle.textContent= 'X';
+})
+var bottomMiddle= document.querySelector('#bottomMiddle')
+
+bottomMiddle.addEventListener('click', function() {
+  bottomMiddle.textContent= 'X';
+})
+var topRight= document.querySelector('#topRight')
+
+topRight.addEventListener('click', function() {
+  topRight.textContent= 'X';
+})
+var bottomRight= document.querySelector('#bottomRight')
+
+bottomRight.addEventListener('click', function() {
+  bottomRight.textContent= 'X';
+})
+var middleRight= document.querySelector('#middleRight')
+
+middleRight.addEventListener('click', function() {
+  middleRight.textContent= 'X';
+})
