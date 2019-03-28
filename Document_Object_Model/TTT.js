db3b2bfaddd463b8fc16c16f93f6316ba1822965
cosa141 <32582921+cosa141@users.noreply.github.com>
2019-03-27 21:32:58 -0500

    almost done with tic tac toe

--- Document_Object_Model/TTT.js
+++ Document_Object_Model/TTT.js
@@ -1,46 +1,67 @@
 var topLeft= document.querySelector('#topLeft')
+var bottomLeft= document.querySelector('#bottomLeft')
+var middleLeft= document.querySelector('#middleLeft')
+var middle= document.querySelector('#middle')
+var topMiddle= document.querySelector('#topMiddle')
+var bottomMiddle= document.querySelector('#bottomMiddle')
+var topRight= document.querySelector('#topRight')
+var bottomRight= document.querySelector('#bottomRight')
+var middleRight= document.querySelector('#middleRight')
 
 topLeft.addEventListener('click', function() {
   topLeft.textContent= 'X';
 })
-
-var middleLeft= document.querySelector('#middleLeft')
-
 middleLeft.addEventListener('click', function() {
   middleLeft.textContent= 'X';
 })
-var bottomLeft= document.querySelector('#bottomLeft')
-
 bottomLeft.addEventListener('click', function() {
   bottomLeft.textContent= 'X';
 })
-var middle= document.querySelector('#middle')
-
 middle.addEventListener('click', function() {
   middle.textContent= 'X';
 })
-var topMiddle= document.querySelector('#topMiddle')
-
 topMiddle.addEventListener('click', function() {
   topMiddle.textContent= 'X';
 })
-var bottomMiddle= document.querySelector('#bottomMiddle')
-
 bottomMiddle.addEventListener('click', function() {
   bottomMiddle.textContent= 'X';
 })
-var topRight= document.querySelector('#topRight')
-
 topRight.addEventListener('click', function() {
   topRight.textContent= 'X';
 })
-var bottomRight= document.querySelector('#bottomRight')
-
 bottomRight.addEventListener('click', function() {
   bottomRight.textContent= 'X';
 })
-var middleRight= document.querySelector('#middleRight')
-
 middleRight.addEventListener('click', function() {
   middleRight.textContent= 'X';
+})
+
+
+
+topLeft.addEventListener('dblclick', function() {
+  topLeft.textContent= 'O';
+})
+middleLeft.addEventListener('dblclick', function() {
+  middleLeft.textContent= 'O';
+})
+bottomLeft.addEventListener('dblclick', function() {
+  bottomLeft.textContent= 'O';
+})
+middle.addEventListener('dblclick', function() {
+  middle.textContent= 'O';
+})
+topMiddle.addEventListener('dblclick', function() {
+  topMiddle.textContent= 'O';
+})
+bottomMiddle.addEventListener('dblclick', function() {
+  bottomMiddle.textContent= 'O';
+})
+topRight.addEventListener('dblclick', function() {
+  topRight.textContent= 'O';
+})
+bottomRight.addEventListener('dblclick', function() {
+  bottomRight.textContent= 'O';
+})
+middleRight.addEventListener('dblclick', function() {
+  middleRight.textContent= 'O';
 })
