92bfdcc44fd5c65ca13f3a045510e5efb8d168ba
cosa141 <32582921+cosa141@users.noreply.github.com>
2019-03-28 21:16:34 -0500

    project solution start

---
+++ Document_Object_Model/gameSolution.js
@@ -0,0 +1,10 @@
+var restart = document.querySelector('#b');
+
+var squares = document.querySelectorAll('td');
+
+
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+}
