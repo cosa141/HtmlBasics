38c456d824d66c07dc7cfea46fa0fcf983a195de
Yang M Rosado <32582921+cosa141@users.noreply.github.com>
2019-04-01 12:17:23 -0500

    working on connect 4

---
+++ jQuery/connect4_master.js
@@ -0,0 +1,45 @@
+var playerOne = prompt('Player One: Enter Your Name, you will be Blue');
+var playerTwo = prompt('Player Two: Enter Your Name, you will be Red');
+var playerOneColor = 'rgb(249, 4, 21)';
+var playerTwoColor = 'rgb(3, 101, 249)';
+
+var game_on = true;
+var table = $('table tr');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function playing() {
+//   var playerOne = prompt('Player One: Enter Your Name, you will be Blue')
+//   var playerTwo = prompt('Player Two: Enter Your Name, you will be Red')
+//   if (playerOne != null){
+//     txt=playerOne+': its your turn'
+//     document.getElementsById("#poo")=txt
+//   }
+// }
+// playing()
+//
+// $('h3').text(playerOne+": it is your turn, please pick a column to drop your blue chip.");
