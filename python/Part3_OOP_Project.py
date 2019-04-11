3c7f161fd4582760c0cb73c4d2dabd7c742ec5a0
cosa141 <32582921+cosa141@users.noreply.github.com>
2019-04-10 20:36:39 -0500

    working on a war game

--- python/Part3_OOP_Project.py
+++ python/Part3_OOP_Project.py
@@ -1,73 +1,47 @@
-#####################################
-### WELCOME TO YOUR OOP PROJECT #####
-#####################################
-
-# For this project you will be using OOP to create a card game. This card game will
-# be the card game "War" for two players, you an the computer. If you don't know
-# how to play "War" here are the basic rules:
-#
-# The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
-# face down. Anyone may deal first. Each player places his stack of cards face down,
-# in front of him.
-#
-# The Play:
-#
-# Each player turns up a card at the same time and the player with the higher card
-# takes both cards and puts them, face down, on the bottom of his stack.
-#
-# If the cards are the same rank, it is War. Each player turns up three cards face
-# down and one card face up. The player with the higher cards takes both piles
-# (six cards). If the turned-up cards are again the same rank, each player places
-# another card face down and turns another card face up. The player with the
-# higher card takes all 10 cards, and so on.
-#
-# There are some more variations on this but we will keep it simple for now.
-# Ignore "double" wars
-#
-# https://en.wikipedia.org/wiki/War_(card_game)
-
 from random import shuffle
 
-# Two useful variables for creating Cards.
 suit = 'H D S C'.split()
 rank = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
 class Deck:
-    """
-    This is the Deck Class. This object will create a deck of cards to initiate
-    play. You can then use this Deck list of cards to split in half and give to
-    the players. It will use SUITE and RANKS to create the deck. It should also
-    have a method for splitting/cutting the deck in half and Shuffling the deck.
-    """
-    suit = 'H D S C'.split()
-    rank = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
     def __init__(self):
+        self.cards=[(s,r) for s in suit for r in rank]
         print('Fresh Deck has been Created!')
-    def freshDeck(self):
-        newdeck=[]
-        for x in self.suit:
-            for y in self.rank:
-                newdeck+=(x,'of',y)
-
+    def __len__(self):
+        return len(self.cards)
+    def showDeck(self):
+        return self.cards
+    def shuffling(self):
+        shuffle(self.cards)
+    def splitHalf(self):
+        return (self.cards[:26],self.cards[26:])
 
 class Hand:
-    '''
-    This is the Hand class. Each player has a Hand, and can add or remove
-    cards from that hand. There should be an add and remove card method here.
-    '''
-    pass
-
+    def __init__(self,card):
+        self.card=card
+    def addCards(self,cardsToAdd):
+        self.card.extend(cardsToAdd)
+    def removeCard(self):
+        return self.card.pop()
+    def __str__(self):
+        return len(self.card)
 class Player:
     """
     This is the Player class, which takes in a name and an instance of a Hand
     class object. The Payer can then play cards and check if they still have cards.
     """
-    pass
+    def __init__(self,playerName):
+        self.playerName=playerName
+    def
 
 
 ######################
 #### GAME PLAY #######
 ######################
 print("Welcome to War, let's begin...")
-print(Deck.cards(rank,suit))
+b=Deck()
+print(b.showDeck())
+print(len(b))
+
+
 # Use the 3 classes along with some logic to play a game of war!
